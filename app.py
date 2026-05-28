81c21c035e33456f3a3c23d17910781c57601606
akashpandeyjd-maker <akashpandey.jd@gmail.com>
2026-05-28 15:06:55 +0530

    app.py

--- app.py
+++ app.py
@@ -1,92 +1,82 @@
 # ---------------------------------------------------
-
 # MAP SECTION
-
 # ---------------------------------------------------
 
 with right:
 
-```
-st.subheader("🗺 Route Map")
+    st.subheader("🗺 Route Map")
 
-# ROUTE LINES
-arc_layer = pdk.Layer(
-    "ArcLayer",
-    data=map_df,
+    # ROUTE LINES
+    arc_layer = pdk.Layer(
+        "ArcLayer",
+        data=map_df,
 
-    get_source_position=["from_lon", "from_lat"],
-    get_target_position=["to_lon", "to_lat"],
+        get_source_position=["from_lon", "from_lat"],
+        get_target_position=["to_lon", "to_lat"],
 
-    get_source_color=[255, 0, 0],
-    get_target_color=[0, 128, 255],
+        get_source_color=[255, 0, 0],
+        get_target_color=[0, 128, 255],
 
-    get_width=4,
+        get_width=4,
 
-    pickable=True,
-    auto_highlight=True
-)
+        pickable=True,
+        auto_highlight=True
+    )
 
-# SOURCE CITY POINTS
-scatter_source = pdk.Layer(
-    "ScatterplotLayer",
-    data=map_df,
+    # SOURCE CITY POINTS
+    scatter_source = pdk.Layer(
+        "ScatterplotLayer",
+        data=map_df,
 
-    get_position=["from_lon", "from_lat"],
+        get_position=["from_lon", "from_lat"],
 
-    get_radius=50000,
+        get_radius=50000,
 
-    get_fill_color=[255, 140, 0],
+        get_fill_color=[255, 140, 0],
 
-    pickable=True
-)
+        pickable=True
+    )
 
-# DESTINATION CITY POINTS
-scatter_destination = pdk.Layer(
-    "ScatterplotLayer",
-    data=map_df,
+    # DESTINATION CITY POINTS
+    scatter_destination = pdk.Layer(
+        "ScatterplotLayer",
+        data=map_df,
 
-    get_position=["to_lon", "to_lat"],
+        get_position=["to_lon", "to_lat"],
 
-    get_radius=50000,
+        get_radius=50000,
 
-    get_fill_color=[0, 128, 255],
+        get_fill_color=[0, 128, 255],
 
-    pickable=True
-)
+        pickable=True
+    )
 
-# INDIA VIEW
-view_state = pdk.ViewState(
-    latitude=22.5937,
-    longitude=78.9629,
-    zoom=4,
-    pitch=35
-)
+    # INDIA VIEW
+    view_state = pdk.ViewState(
+        latitude=22.5937,
+        longitude=78.9629,
+        zoom=4,
+        pitch=35
+    )
 
-# MAP
-deck = pdk.Deck(
-    map_style="light",
+    # MAP
+    deck = pdk.Deck(
+        initial_view_state=view_state,
 
-    initial_view_state=view_state,
+        layers=[
+            arc_layer,
+            scatter_source,
+            scatter_destination
+        ],
 
-    layers=[
-        arc_layer,
-        scatter_source,
-        scatter_destination
-    ],
+        tooltip={
+            "html": "<b>Source:</b> {from_name}<br/><b>Destination:</b> {to_name}<br/><b>Distance:</b> {distance} KM",
 
-    tooltip={
-        "html": """
-        <b>Source:</b> {from_name}<br/>
-        <b>Destination:</b> {to_name}<br/>
-        <b>Distance:</b> {distance} KM
-        """,
-
-        "style": {
-            "backgroundColor": "black",
-            "color": "white"
+            "style": {
+                "backgroundColor": "black",
+                "color": "white"
+            }
         }
-    }
-)
+    )
 
-st.pydeck_chart(deck)
-```
+    st.pydeck_chart(deck)
